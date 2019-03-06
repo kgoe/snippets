82831a07f8ee47ba9121dbc423c7b7ba7845689f
kgoespiritu <kgoespiritu@gmail.com>
2019-03-06 08:18:26 +0800

    window close

---
+++ js/window-close.js
@@ -0,0 +1,13 @@
+/*
+<form>
+<input type=button value="Close Window" onClick="javascript:window.close();">
+</form> 
+*/
+
+/*
+<a href="javascript:window.close();">Click to Close Window</a>
+*/
+
+/*
+<BODY onLoad="setTimeout(window.close,20000)">
+*/
