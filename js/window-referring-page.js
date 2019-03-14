812dbc3e27f5f5d8c1ba035785bc3a03401487d7
kgoespiritu <kgoespiritu@gmail.com>
2019-03-14 08:28:48 +0800

    window referrer

---
+++ js/window-referring-page.js
@@ -0,0 +1,5 @@
+var allowedreferrer = "http://www.yoursite.com/referringpagename.htm"; 
+if (document.referrer.indexOf(allowedreferrer) == -1) {
+	alert("You can only access this page from " + allowedreferrer);
+	window.location=allowedreferrer;
+}
