276c56ab1eb5c39a4b20bc8681ac9e47d38de64b
kgoespiritu <kgoespiritu@gmail.com>
2019-03-17 08:31:33 +0800

    adding snippet link

--- js/snippets-links.js
+++ js/snippets-links.js
@@ -7,4 +7,5 @@ doc.links =
 https://www.htmlgoodies.com/beyond/javascript/article.php/3887346
 https://html-online.com/articles/common-javascript-jquery-code-snippets/
 https://onextrapixel.com/10-useful-and-time-saving-javascript-snippets/
+https://codetogo.io/
 `;
