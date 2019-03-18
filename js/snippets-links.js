f8543962005dc63502ce31ef6f66d201793d4ce1
kgoespiritu <kgoespiritu@gmail.com>
2019-03-18 08:32:11 +0800

    adding snippet link

--- js/snippets-links.js
+++ js/snippets-links.js
@@ -8,4 +8,6 @@ https://www.htmlgoodies.com/beyond/javascript/article.php/3887346
 https://html-online.com/articles/common-javascript-jquery-code-snippets/
 https://onextrapixel.com/10-useful-and-time-saving-javascript-snippets/
 https://codetogo.io/
+
+https://marketplace.visualstudio.com/items?itemName=lndgalante.ReactSnippetsCommonJS
 `;
