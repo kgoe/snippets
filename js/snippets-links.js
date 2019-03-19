766894705d240b2bf0741260a3c366d76eb766bf
kgoespiritu <kgoespiritu@gmail.com>
2019-03-19 08:33:14 +0800

    adding snippet link

--- js/snippets-links.js
+++ js/snippets-links.js
@@ -8,6 +8,10 @@ https://www.htmlgoodies.com/beyond/javascript/article.php/3887346
 https://html-online.com/articles/common-javascript-jquery-code-snippets/
 https://onextrapixel.com/10-useful-and-time-saving-javascript-snippets/
 https://codetogo.io/
+https://css-tricks.com/snippets/
+https://www.producthunt.com/posts/code-to-go
+
+https://vanillajstoolkit.com/
 
 https://marketplace.visualstudio.com/items?itemName=lndgalante.ReactSnippetsCommonJS
 `;
