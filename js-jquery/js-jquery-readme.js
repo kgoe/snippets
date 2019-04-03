76485175bbdfd431545b34b33decd8e1a0157375
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-04-03 15:51:22 +0800

    jquery selectors

--- js-jquery/js-jquery-readme.js
+++ js-jquery/js-jquery-readme.js
@@ -8,10 +8,302 @@ jQuery API
 
 doc.jquery_api_index=
 `
+ajax
+attributes
+callbacks object
+core
+css
+data
+deferred object
+dimensions
+effects
+events
+forms
+internals
+manipulation
+miscellaneous
+offset
+properties
+selectors
+deprecated
+removed
+`;
+
+doc.jquery_api_index_level_2=
+`
+ajax
+attributes
+callbacks object
+core
+css
+data
+deferred object
+dimensions
+effects
+> basic
+> custom
+> fading
+> sliding
+events
+> browser events
+> document loading
+> event handler attachment
+> event object
+> form event
+> keyboard events
+> mouse events
+forms
+internals
+manipulation
+> class attributes
+> copying
+> dom insertion, around
+> dom insertion, inside
+> dom insertion, outside
+> dom removal
+> dom repplacement
+> general attributes
+> style properties
+miscellaneous
+> collection manipulation
+> data storage
+> dom element methods
+> setup methods
+offset
+properties
+> properties of jquery object instances
+> properties of the global jquery object
+selectors
+> attribute
+> basic
+> basic filter
+> content filter
+> form
+> hierarchy
+> jquery extensions
+> visibility filter
+traversing
+> filtering
+> miscellaneous traversing
+> tree traversal
+utilities
+version
+deprecated
+removed
+`;
+
+doc.jquery_ajax=
+`
+`;
+
+doc.jquery_attributes=
+`
+`;
+
+doc.jquery_callbacks_object=
+`
+`;
+
+doc.jquery_core=
+`
+`;
+
+doc.jquery_css=
+`
+`;
+
+doc.jquery_data=
+`
+`;
+
+doc.jquery_deferred_object=
+`
+`;
+
+doc.jquery_dimensions=
+`
+`;
+
+doc.jquery_effects=
+`
+`;
+
+doc.jquery_events=
+`
+`;
+
+doc.jquery_forms=
+`
+`;
+
+doc.jquery_internals=
+`
+# .context
+.jquery
+jQuery.error()
+.pushStack()
+# .selector
+`;
+
+doc.jquery_manipulation=
+`
+`;
+
+doc.jquery_miscellaneous=
+`
+`;
+
+doc.jquery_offset=
+`
+`;
+
+doc.jquery_properties=
+`
+`;
+
+doc.jquery_selectors=
+`
+*
+:animated
+[name|="value"] ( Attribute Contains Prefix Selector )
+[name*="value"] ( Attribute Contains Selector )
+[name~="value"] ( Attribute Contains Word Selector )
+[name$="value"] ( Attribute Ends With Selector )
+[name="value"]  ( Attribute Equals Selector )
+[name!="value"] ( Attribute Not Equal Selector )
+[name^="value"] ( Attribute Starts With Value )
+:button
+:checkbox
+:checked
+parent>child ( Child Selector )
+.class ( Class Selector )
+:contains("searchtext")
+ancestor descentant ( Descendant Selector )
+:disabled
+element ( Element Selector )
+:empty
+:enabled
+:eq(n), :eq(-n) ( n-th Index Element Selector )
+:event
+:file
+:first-child
+:first-of-type
+:first
+:focus
+:gt(n) ( Greater Than Index Selector )
+[name] ( Has Attribute Name Selector )
+:has(selector) ( Has Selector )
+:header
+:hidden
+#my_id ( ID Selector )
+:image
+:input
+:lang()
+:last-child
+:last-of-type
+:last
+:lt(n) ( Less Than Index Selector )
+[name="value"][name2="value2"] ( Multiple Attributes Selector )
+selector1, selector2, selectorN ( Multiple Selectors )
+prev + next ( Next Adjacent Element Selector )
+prev ~ siblings ( Next Siblings Selector )
+:not(selector) ( Not Selector )
+:nth-child(n)
+:nth-last-child(n)
+:nth-of-type(n)
+:nth-last-of-type(n)
+:odd
+:only-child
+:only-of-type
+:parent
+:password
+:radio
+:reset
+:root
+:selected
+:submit
+:target
+:text
+:visible
+`;
+
+doc.jquery_traversing=
+`
+.add()
+.addBack()
+# .andSelf()
+.children()
+.closest()
+.contents()
+.each()
+.end()
+.eq()
+.filter()
+.find()
+.first()
+.has()
+.is()
+.last()
+.map()
+.next()
+.nextAll()
+.nextUntil()
+.not()
+.offsetParent()
+.parent()
+.parents()
+.parentUntil()
+.prev()
+.prevAll()
+.prevUntil()
+.siblings()
+.slice()
+`;
+
+doc.jquery_utilities=
+`
+.clearQueue()
+.dequeue()
+# jQuery.boxModel
+# jQuery.browser
+jQuery.contains()
+jQuery.data()
+jQuery.dequeue()
+jQuery.each()
+jQuery.extend()
+jQuery.fn.extend()
+jQuery.globalEval()
+jQuery.grep()
+jQuery.inArray()
+jQuery.isArray()
+jQuery.isEmptyObject()
+# jQuery.isFunction()
+jQuery.isNumeric()
+jQuery.isPlainObject()
+# jQuery.isWindow()
+jQuery.isXMLDoc()
+jQuery.makeArray()
+jQuery.map()
+jQuery.merge()
+jQuery.noop()
+jQuery.now()
+jQuery.parseHTML()
+# jQuery.parseJSON()
+jQuery.parseXML()
+jQuery.proxy()
+jQuery.queue()
+jQuery.removeData()
+# jQuery.support
+jQuery.trim()
+# jQuery.unique()
+jQuery.uniqueSort()
+.queue()
 `;
 
 doc.links=
 `
+REFERENCE LINKS:
+
 https://jquery.com/
 https://api.jquery.com/
 https://jqueryui.com/
