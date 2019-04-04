7bb3ede471d21607f8951a5f3eeeb6850b6028d2
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-04-04 15:16:16 +0800

    load jquery if not loaded

---
+++ js/window-load-jquery.js
@@ -0,0 +1,7 @@
+;doc={};
+
+if( typeof jQuery == 'undefined' ) {
+  var jquery_1_7_1 = '<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.1.min.js"></'+'script>';
+  var jquery_1_10_2 = '<script src="https://code.jquery.com/jquery-1.10.2.js"></'+'script>';
+  document.write(jquery_1_10_2);
+}
