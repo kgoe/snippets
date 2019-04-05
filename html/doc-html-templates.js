c72f5a79529c4893ac74726c8281c9cb823fbd45
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-04-05 19:13:06 +0800

    templaates

---
+++ html/doc-html-templates.js
@@ -0,0 +1,9 @@
+;doc={};
+
+doc.sort=
+`
+https://www.w3schools.com/howto/default_page6.asp
+https://stackoverflow.com/questions/4611591/code-vs-pre-vs-samp-for-inline-and-block-code-snippets
+https://code.visualstudio.com/docs/languages/html
+https://bootsnipp.com/
+`;
