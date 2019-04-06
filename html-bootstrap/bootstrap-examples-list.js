0698c85a4244f2017705dea949dc0e56456dca15
kgoe-mobile <kgoespiritu@gmail.com>
2019-04-06 17:29:01 +0800

    template list

---
+++ html-bootstrap/bootstrap-examples-list.js
@@ -0,0 +1,70 @@
+;doc={};
+
+doc.bootstrap3examples=
+`
+01|
+02|
+03|
+04|
+05|
+06|
+07|
+08|
+09|
+10|
+11|
+12|
+13|
+14|
+15|
+16|
+17|
+18|
+19|
+20|
+
+blog
+carousel
+cover
+dashboard
+grid
+jumbotron
+jumbotron-narrow
+justified-nav
+navbar
+navbar-fixed-top
+navbar-static-top
+non-responsive
+offcanvas
+screenshots
+signin
+starter-template
+sticky-footer
+sticky-footer-navbar
+theme
+tooltip-viewport
+`;
+
+doc.bootstrap4examples=
+`
+01|album
+02|pricing
+03|checkout
+04|product
+05|cover
+06|carousel
+07|blog
+08|dashboard
+09|sign in
+10|sticky footer
+11|sticky footer navbar
+12|starter template
+13|grid
+14|jumbotron
+15|navbars
+16|navbar static
+17|navbar fixed
+18|navbar bottom
+19|floating labels
+20|offcanvas
+`;
