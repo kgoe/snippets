a308ce8b3ce572abdf2e3f95dfefcae3d069a7d1
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-04-08 17:20:58 +0800

    javascript reference patterns

---
+++ javascript-reference-patterns.js
@@ -0,0 +1,231 @@
+;doc={};
+
+/**
+ * template
+ */
+((doc)=>{
+})(doc);
+
+doc.sort=
+`
+R001|essential-js-design-patterns|https://github.com/addyosmani/essential-js-design-patterns
+R002|javascript-design-patterns-for-humans|https://github.com/kamranahmedse/design-patterns-for-humans
+R003|JavaScript-Design-Patterns|http://robdodson.me/blog/2012/08/03/javascript-design-patterns/
+R004|Design-Patterns-JS|https://coveralls.io/github/FelipeBB/Design-Patterns-JS
+R010|JavaScript-Templates|https://blueimp.github.io/JavaScript-Templates/
+R200|Design-Patterns-in-Javascript-ES6
+`;
+
+/**
+ * R001
+ */
+((doc)=>{
+  doc.R001=
+`
+Introduction
+What is a Pattern
+Patternity Testing
+The Structure of a Design Pattern
+Anti-Patterns
+Categories of Design Pattern
+Summary Table of Design Pattern Categoriezation
+JavaScript Design Patterns
+- Contructor
+- Module Pattern
+- Revealing Module Pattern
+- Singleton Pattern
+- Observer Pattern
+- Mediator Pattern
+- Prototype Pattern
+- Command Pattern
+- Facade Pattern
+- Factory Pattern
+- Mixin Pattern
+- Decorator Pattern
+- Flyweight Pattern
+JavaScript MV* Patterns
+- MVC Patterns
+- MVP Patterns
+- MVVM Patterns
+Modern Modular JavaScript
+- AMD
+- CommonJS
+- ES Harmony
+Design Patterns in jQuery
+- Composite Pattern
+- Adapter Pattern
+- Facade Pattern
+- Observer Pattern
+- Iterator Pattern
+- Lazy Initialization Pattern
+- Proxy Pattern
+- Builder Pattern
+jQuery Plugin Patterns
+JavaScript Namespacing Patterns
+Conclusion
+References
+
+`;
+})(doc);
+
+/**
+ * R002
+ */
+((doc)=>{
+  doc.R002=
+`
+creational
+- simple factory
+- factory method
+- abstract factory
+- builder
+- prototype
+- singleton
+structural
+- adapter
+- bridge
+- composite
+- decorator
+- facade
+- flyweight
+- proxy
+behavioral
+- chain of responsibility
+- command
+- iterator
+- mediator
+- memento
+- observer
+- visitor
+- strategy
+- state
+- template method
+`;
+})(doc);
+
+/**
+ * R003
+ */
+((doc)=>{
+  doc.R003=
+`
+creational
+- abstract factory
+- builder
+- factory method
+- object pool
+- prototype
+structural
+- adapter
+- bridge
+- composite
+- decorator
+- facade
+- flyweight
+- private class data
+- proxy
+behavioral
+- chain of respponsibility
+- command
+- interpreter
+- iterator
+- mediator
+- memento
+- null object
+- observer
+- state
+- strategy
+- template method
+- visitor
+- monad pattern
+- promises
+
+decorator
+factory
+iterator
+observer
+singleton
+strategy
+`;
+})(doc);
+
+/**
+ * R004
+ */
+((doc)=>{
+  doc.R004=
+`
+creational patterns
+- abstract factory
+- builder
+- factory method
+- prototype
+- singleton
+structural patterns
+- adapter
+- bridge
+- composite
+- decorator
+- facade
+- flyweight
+- proxy
+behavioral patterns
+- chain of responsibility
+- command
+- interpreter
+- iterator
+- mediator
+- observer
+- state
+- strategy
+- template
+- visitor
+`;
+})(doc);
+
+/**
+ * R010
+ */
+((doc)=>{
+  doc.R010=
+`
+client side:tmpl.min.js
+`;
+})(doc);
+
+/**
+ * R200
+ */
+((doc)=>{
+  doc.R200=
+`
+Design Pattern in JavaScript - ES6
+Adapter
+Builder
+Bridge
+Chaining
+Command
+Composite
+Composite Iterator
+Compound (Use some design patterns together to solve a problem)
+Decorator
+Facade
+Factory
+Iterator
+Lazy
+Module
+Module Revealed
+Multi-Inheritance ES6
+MVC
+Namespace
+Nullify
+Observer
+Prototype
+Proxy
+Singleton
+State
+Strategy
+Template
+Try-Finally
+`;
+})(doc);
