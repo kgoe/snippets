e643b5945a49a95a9cfc2949dd6740b523135c07
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-04-08 19:44:13 +0800

    python

---
+++ prg-python/doc-python-001.js
@@ -0,0 +1,149 @@
+;doc={};
+
+doc.name=
+`
+python 3.x
+python 2.x
+`;
+
+doc.python_topics=
+`
+binary data
+build-in constants
+built-in exceptions
+built-in functions
+built-in types
+concurrent types
+context variables
+cryptography
+data compression
+data persistence
+data types
+debugging & profiling
+development tools
+email
+file and directory access
+file formats
+frameworks
+functional programming
+importing
+input / output
+internationalization
+internet
+internet data
+interpreters
+language
+logging
+ms windows
+multimedia
+networking & interprocess communication
+numeric & amthematical
+operating system
+runtime
+software packaging & distribution
+structured markup
+text processing
+tk
+unix
+`;
+
+doc.python_topics_details=
+`
+binary data
+- codecs
+- encodings
+- struct
+built-in constants
+- Constants
+- copyright
+- credits
+- Ellipsis
+- exit
+- False
+- license
+- None
+- NotImplemented
+- quit
+- True
+- __debug__
+built-in exceptions
+- ArithmeticError
+- AssertionError
+- AttributeError
+- BaseException
+- BlockingIOError
+- BrokenPipeError
+- BufferError
+- BytesWarning
+- ChildProcessError
+- ConnectionAbortedError
+- ConnectionError
+- ConnectionRefusedError
+- ConnectionResetError
+- DeprecationWarning
+- EnvironmentError
+- EOFError
+- Exception
+- Exceptions
+- FileExistsError
+- FileNotFoundError
+- FloatingPointError
+- FutureWarning
+- GeneratorExit
+- ImportError
+- ImportWarning
+- IndentationError
+- IndexError
+- InterruptedError
+- IOError
+- IsADirectoryError
+- KeyboardInterrupt
+- KeyError
+- LookupError
+- MemoryError
+- ModuleNotFoundError
+- NameError
+- NotADirectoryError
+- NotImplementedError
+- OSError
+- OverflowError
+- PendingDeprecationWarning
+- PermissionError
+- ProcessLookupError
+- RecursionError
+- ReferenceError
+- ResourceWarning
+- RuntimeError
+- RuntimeWarning
+- StopAsyncIteration
+- StopIteration
+- SyntaxError
+- SyntaxWarning
+- SystemError
+- SystemExit
+- TabError
+- TimeoutError
+- TypeError
+- UnboundLocalError
+- UnicodeDecodeError
+- UnicodeEncodeError
+- UnicodeError
+- UnicodeWarning
+- UserWarning
+- ValueError
+- Warning
+- WindowsError
+- ZeroDivisionError
+built-in functions
+built-in types
+- bytearray
+networking & interprocess communication
+- asynchat|deprecated
+- asyncore|deprecated
+- asyncio
+- mmap
+- signal
+- socket
+- ssl
+`;
+
