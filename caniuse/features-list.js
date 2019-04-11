8bd22a5abcef64b6bd8e6f59a7de72b9c8e199d3
kgoespiritu <kgoespiritu@gmail.com>
2019-04-12 07:43:47 +0800

    caniuse lists

---
+++ caniuse/features-list.js
@@ -0,0 +1,20 @@
+;doc={};
+
+doc.name=
+`
+caniuse features
+`;
+
+doc.link=
+`
+https://caniuse.com/
+https://github.com/fyrd/caniuse
+
+http://www.evolutionoftheweb.com/
+http://btoe.ws/browserstats/#stats/css-canvas.json
+http://html5readiness.com/
+`;
+
+doc.list=
+`
+`;
---
+++ caniuse/features-test.js
@@ -0,0 +1,89 @@
+;doc={};
+
+doc.name=
+`
+`;
+
+doc.link=
+`
+https://tests.caniuse.com/
+`;
+
+doc.list=
+`
+Resource Hints: dns-prefetch
+Text API for Canvas
+WebGL - 3D Canvas graphics
+Canvas blend modes
+WebGL 2.0
+CSS image-set
+CSS background-attachment
+CSS Appearance
+CSS caret-color
+CSS Containment
+CSS position:fixed
+CSS font-stretch
+CSS hanging-punctuation
+CSS Initial Letter
+CSS initial value
+letter-spacing CSS property
+CSS Motion Path
+CSS overflow property
+CSS page-break properties
+Rebeccapurple color
+CSSOM Scroll-behavior
+CSS Scroll snap
+CSS position:sticky
+CSS Subgrid
+CSS text-justify
+CSS text-orientation
+CSS touch-action property
+CSS widows & orphans
+CSS font-size-adjust
+CSS font-variant-numeric
+CSS user-select: none
+CSS will-change property
+CSS Masks
+::placeholder CSS pseudo-element
+:placeholder-shown CSS pseudo-class
+CSS :read-only and :read-write selectors
+system-ui value for font-family
+Variable fonts
+CSS all property
+CSS background-blend-mode
+Case-insensitive CSS attribute selectors
+
+KeyboardEvent.which [unoff]
+Custom Elements (deprecated V0 spec) [unoff]
+seamless attribute for iframes [unoff]
+Offline web applications [unoff]
+Scoped CSS [unoff]
+Context menu item (menuitem element) [unoff]
+flat & flatMap array methods [unoff]
+Object.observe data binding [unoff]
+Background Sync API [unoff]
+document.evaluate & XPath [unoff]
+Document.execCommand() [unoff]
+Filesystem & FileWriter API [unoff]
+Network Information API [unoff]
+Permissions API [unoff]
+Picture-in-Picture [unoff]
+Resize Observer [unoff]
+Efficient Script Yielding: setImmediate() [unoff]
+Speech Recognition API [unoff]
+Web SQL Database [unoff]
+Web Bluetooth [unoff]
+Web Share API [unoff]
+WebUSB [unoff]
+WebXR API [unoff]
+Directory selection from file input [unoff]
+Speech Synthesis API [unoff]
+HTTP Live Streaming (HLS) [unoff]
+WebP image format [unoff]
+SPDY protocol [unoff]
+EOT - Embedded OpenType fonts [unoff]
+XHTML+SMIL animation [unoff]
+Animated PNG (APNG) [unoff]
+Feature Policy [unoff]
+SVG fonts [unoff]
+`;
