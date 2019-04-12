5d72fb7f4a66d3b9e5412ac5f7723f61fa9628b2
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-04-12 12:21:05 +0800

    updates

--- python/python-topics.js
+++ python/python-topics.js
@@ -12,29 +12,30 @@ method
 list = [1,2,3]
 dictinary = {'a':1, 'b':2, 'c':3}
 tuple = (1,2,3)
+set = set()
 `;
 
 dev.udemy_complete_python_bootcamp=
 `
 udemy complete python bootcamp
-1. course overview
-2. python setup
-3. python object and data structure basics
-4. python comparison operators
-5. python statements
-6. methods and functions
-7. milestone project 1
-8. object oriented programming
-9. modules and packages
-10. errors and exceptions handling
-11. milestone project 2
-12. python decorators
-13. python generators
-14. final capstone python project
-15. advanced python modules
-16. advanced python objects and data structures
-17. bonus material - introduction to guis
-18. bonus special offers
-19. appendix : older python 2 material
-20. bonus coupons for other courses
+1. course overview | 4
+2. python setup | 5
+3. python object and data structure basics | 36
+4. python comparison operators | 3
+5. python statements | 7
+6. methods and functions | 24
+7. milestone project 1 | 5
+8. object oriented programming | 9
+9. modules and packages | 3
+10. errors and exceptions handling | 5
+11. milestone project 2 | 5
+12. python decorators | 2
+13. python generators | 3
+14. final capstone python project | 1
+15. advanced python modules | 10
+16. advanced python objects and data structures | 7
+17. bonus material - introduction to guis | 7
+18. bonus special offers | 1
+19. appendix : older python 2 material | 74
+20. bonus coupons for other courses | 1
 `;
