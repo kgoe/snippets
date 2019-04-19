30b1433383d77893121612271cf9322b2184d7b0
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-04-19 17:54:20 +0800

    python topics

--- shell-python/python-topics.js
+++ shell-python/python-topics.js
@@ -47,17 +47,93 @@ exampleSet2 = set([1,2,3,4])
 File I/O
 `;
 
-doc.devdocs_python2=
+doc.devdoc_python2=
 `
+Devdocs Python 2|https://devdocs.io/python~2.7/
+built-in constants|12
+built-in exceptions|56
+built-in functions|81
+built-in types|130
+compiler|14
+crypography|34
+data compression|127
+data persistence|125
+data types|351
+debugging & profiling|109
+development tools|177
+file & directory access|123
+fole formats|123
+frameworks|45
+importing|66
+input/output|29
+internationalization|53
+internet|654
+internet data|448
+interpreters|18
+language|135
+logging|126
+mac os|140
+ms widows|104
+multimedia|152
+networking|211
+numeric & mathematical|391
+operating system|622
+optional operating system|224
+restricted execution|25
+runtime|201
+sgi irix|104
+software packaging & distribution|4
+string|238
+structured markup|372
+tk|234
+unix|79
 `;
 
-doc.devdocs_python3=
+doc.devdoc_python3=
 `
+Devdocs Python 3|https://devdocs.io/python~3.7/
+binary data|73
+built-in constants|12
+built-in exceptions|82
+built-in functions|70
+built-in types|206
+concurrent execution|301
+context variables|18
+cryptography|37
+data compression|147
+data persistence|114
+data types|360
+debugging & profiling|156
+development tools|399
+email|285
+file & directory access|197
+file formates|125
+frameworks:169
+functional programming|133
+importing|162
+input/output|335
+internationalization|56
+internet|840
+internet data|216
+interpreters|18
+language|162
+logging|150
+ms windows|105
+multimedia|139
+network & interprocess communication|267
+numeric & mathematical|279
+operating system|703
+runtime|298
+software packaging & distribution|17
+structured markup|349
+text processing|180
+tk|122
+unix|63
 `;
 
-doc.udemy_complete_python_bootcamp=
+doc.udemy_complete_python3_bootcamp=
 `
-udemy complete python bootcamp
+udemy complete python bootcamp|https://www.udemy.com/complete-python-bootcamp
 1. course overview | 4
 2. python setup | 5
 3. python object and data structure basics | 36
