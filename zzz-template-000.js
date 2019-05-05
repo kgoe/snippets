b7c99cdb5f77547027536c707b6c3a8165b06b0d
kelvin.espiritu <kelvin.espiritu@gmanmi.com>
2019-05-05 17:34:26 +0800

    template snippet

---
+++ zzz-template-000.js
@@ -0,0 +1,36 @@
+;
+
+doc = {};
+
+doc.name =
+`
+`;
+
+doc.desc =
+`
+`;
+
+doc.toc =
+`
+`;
+
+doc.topics =
+`
+`;
+
+doc.links =
+`
+`;
+
+doc.sort =
+`
+`;
+
+
+
+
+
+
+
+
+
---
+++ zzz-template-001.js
@@ -0,0 +1,46 @@
+;doc={};
+
+/**
+ * Template
+ */
+((doc)=>{
+doc.template=
+`
+`;
+})(doc);
+
+/**
+ * name
+ */
+((doc)=>{
+doc.name=
+`
+`;
+})(doc);
+
+/**
+ * desc
+ */
+((doc)=>{
+doc.desc=
+`
+`;
+})(doc);
+
+/**
+ * links
+ */
+((doc)=>{
+doc.links=
+`
+`;
+})(doc);
+
+/**
+ * sort
+ */
+((doc)=>{
+doc.sort=
+`
+`;
+})(doc);
